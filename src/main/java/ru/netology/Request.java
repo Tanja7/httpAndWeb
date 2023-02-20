8f122b052d7312c62f481fd713439448fb83e7e7
Tanja7 <109756465+Tanja7@users.noreply.github.com>
2023-02-20 14:10:30 +0300

    2 задача

--- src/main/java/ru/netology/Request.java
+++ src/main/java/ru/netology/Request.java
@@ -1,34 +1,149 @@
 package ru.netology;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
 public class Request {
-    //метод запроса, потому что на разные методы можно назначить один и тот же хендлер;
+    private static final URLEncodedUtils URL_ENCODED_UTILS = new URLEncodedUtils();
+    // хранение избыточно - выполнено для реализации ДЗ Метод: getQueryParams().
+    private final String stringHeaders; // строка обработчика
+    private final String[] requestLine; // линия запроса
+    private final Map<String, String> headers; // обработчики
+
+    //тело запроса, если есть.
+    private final byte[] body;
+    // метод запроса
     private final String method;
-//заголовки запроса;
-    private final String header;
+    //    путь запроса
+    private final String path;
 
-//тело запроса, если есть.
-    private String body;
-
-    public Request (String method, String header, String body) {
-        this.method = method;
-        this.header = header;
+    private Request(String[] requestLine, String stringHeaders, Map<String, String> headers, byte[] body) {
+        this.stringHeaders = stringHeaders;
+        this.requestLine = requestLine;
+        this.headers = headers;
         this.body = body;
+        method = requestLine[0];
+        path = requestLine[1];
     }
-    public Request (String method, String header) {
-        this.method = method;
-        this.header = header;
 
+    // получение параметров из Query String
+    public List<NameValuePair> getQueryParams() throws IOException {
+        return getListQueryParams(stringHeaders);
+    }
+
+    // получение пути запроса.
+    public String getQueryParam(String headerName) {
+        return headers.getOrDefault(headerName, null);
+    }
+
+    public String[] getRequestLine() {
+        return requestLine;
+    }
+
+    public byte[] getBody() {
+        return body;
     }
 
     public String getMethod() {
         return method;
     }
 
-    public String getHeader() {
-        return header;
+    public String getPath() {
+        return path;
     }
 
-    public String getBody() {
-        return body;
+    public Map<String, String> getHeaders() {
+        return headers;
     }
+
+    // путь без Query
+    public String getCleanPath() {
+        return this.path.split("\\?")[0];
+    }
+
+    //запрос из входного потока
+    public static Request requestFromInputStream(InputStream inputStream) throws IOException {
+
+        var in = new BufferedReader(new InputStreamReader(inputStream));
+
+        // читаем request line
+        // проверка частей запроса
+        final var REQUEST_LINE = in.readLine().split(" ");
+        if (REQUEST_LINE.length != 3) {
+            throw new IOException("REQUEST_LINE.length !=3!");
+        }
+        // проверка метода запроса
+        final var METHOD = REQUEST_LINE[0];
+        if (!Main.allowedMethods.contains(METHOD)) {
+            throw new IOException("415 Method not support.");
+        }
+        // проверка пути
+        final var path = REQUEST_LINE[1];
+        if (!path.startsWith("/")) {
+            throw new IOException("PATH starts not with '/'.");
+        }
+
+        // ищем заголовки
+        final var STRING_HEADERS = in.readLine();
+        final var LIST_NAME_VALUE_PAIR = getListQueryParams(STRING_HEADERS);
+        final var HEADERS = headersLitToMap(LIST_NAME_VALUE_PAIR);
+
+        //Ищем тело, в случае наличия
+        //  * пропускаем пустую строку
+        in.readLine();
+        final byte[] BODY =
+                (!(REQUEST_LINE[0].equals(Main.GET)) && HEADERS.containsKey("Content-Length")) ?
+                        in.readLine().getBytes(StandardCharsets.UTF_8) : null;
+
+        return new Request(REQUEST_LINE, STRING_HEADERS, HEADERS, BODY);
+    }
+
+//NameValuePair - это интерфейс, который определяется в http-клиенте apache
+// List<NameValuePair> это список <key, value> пары которые будут использоваться
+// в качестве параметров в запросе http post
+
+    private static List<NameValuePair> getListQueryParams(String queryString) {
+        return URL_ENCODED_UTILS.parse
+                (queryString, StandardCharsets.UTF_8);
+    }
+
+    // заголовки, отображаемые в маре
+    private static Map<String, String> headersLitToMap(List<NameValuePair> headers) {
+        Map<String, String> mapHeaders = new HashMap<>();
+        for (NameValuePair line : headers) {
+            mapHeaders.put(line.getName(), line.getValue());
+        }
+        return mapHeaders;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Request) obj;
+        return Arrays.equals(this.requestLine, that.requestLine) &&
+                Objects.equals(this.headers, that.headers) && Objects.equals(this.body, that.body);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(requestLine);
+    }
+
+    @Override
+    public String toString() {
+        return "Request[" +
+                "method=" + method + ", " +
+                "path=" + path + ", " +
+                "headers=" + headers + "]";
+    }
+
 }
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -1,25 +1,136 @@
 package ru.netology;
-import java.io.IOException;
+
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
-    private static final int port = 9999;
-    private final ExecutorService threadPool = Executors.newFixedThreadPool(64);
 
-    public void start() {
-        // стартуем сервер
-        try (var serverSocket = new ServerSocket(port)) {
-            //  принимаем подключения
-            while (true) {
-                // обработка одного подключения
-                var socket = serverSocket.accept();
-                var thread = new Handler(socket);
-                threadPool.submit(thread);
+    // Список разрешённых путей
+    private static final List<String> validPaths = Main.validPaths;
+    // Список разрешенных методов
+    private static final List<String> allowedMethods = Main.allowedMethods;
+    //
+    private final HashMap<String, HashMap<String, Handler>> HANDLERS = Main.handlers;
+
+    // Map обработчиков, включает key(метод):value(Map key(путь):value(обработчик))
+    private final HashMap<String, HashMap<String, Handler>> handlers;
+    private final ExecutorService threadPool;
+
+    public Server() {
+        threadPool = Executors.newFixedThreadPool(64);
+        handlers = HANDLERS;
+
     }
-            } catch(IOException e){
-                e.getMessage();
+
+    @SuppressWarnings("InfiniteLoopStatement")
+//    public void serverOn(int port)
+    public void start(int port) {
+        try (final var serverSocket = new ServerSocket(port)) {
+            while (true) {
+                final var socket = serverSocket.accept();
+                threadPool.submit(new Client(socket));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addHandler(String method, String path, Handler handler) {
+        handlers
+                .computeIfAbsent(method, k -> new HashMap<>())
+                .computeIfAbsent(path, k -> handler);
+    }
+
+    private class Client extends Thread {
+        final Socket clientSocket;
+        final InputStream in;
+        final BufferedOutputStream out;
+
+        public Client(Socket clientSocket) throws IOException {
+            this.clientSocket = clientSocket;
+            this.in = clientSocket.getInputStream();
+            this.out = new BufferedOutputStream(clientSocket.getOutputStream());
+        }
+
+        private void badRequest(Request request, BufferedOutputStream out) throws IOException {
+            try {
+
+                final var filePath = Path.of(".", "public", "/bad-request.html");
+                final var mimeType = "html";
+
+                final var template = Files.readString(filePath);
+                final var contentReplaceTime = template.replace(
+                        "{time}",
+                        LocalDateTime.now().toString()
+                );
+
+                final var content = contentReplaceTime
+                        .replace("{response}", request.getPath())
+                        .getBytes(StandardCharsets.UTF_8);
+                out.write((
+                        "HTTP/1.1 404 Bad request\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + content.length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.write(content);
+                out.flush();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void defaultCase(Path filePath, String mimeType) throws IOException {
+            final var length = Files.size(filePath);
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            Files.copy(filePath, out);
+            out.flush();
+        }
+
+        @Override
+        public void run() {
+            try {
+
+                Request request = Request.requestFromInputStream(in);
+                System.out.println(request.getMethod() + " - МЕТОД");
+                System.out.println(request + "\n");
+
+                if (Main.handlers.getOrDefault(request.getMethod(), null)
+                        .getOrDefault(request.getCleanPath(), null) != null) {
+
+                    Main.handlers.get(request.getMethod()).get(request.getCleanPath())
+                            .handle(request, out);
+                } else if (Main.validPaths.contains(request.getCleanPath())) {
+                    final var filePath = Path.of(".", "public", request.getPath());
+                    final var mimeType = Files.probeContentType(filePath);
+                    defaultCase(filePath, mimeType);
+
+                } else badRequest(request, out);
+
+                clientSocket.close();
+                in.close();
+                out.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
+}
